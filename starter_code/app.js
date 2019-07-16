8fb3bedb1153704d8abbf17ffedef4be46d93db6
Marius Grondahl <marius@grondahldesign.no>
2019-07-16 13:01:58 +0200

    First commit

--- starter_code/app.js
+++ starter_code/app.js
@@ -5,6 +5,8 @@ const app     = express();
 const path    = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+hbs.registerPartials(__dirname + '/views/partials');
+
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
@@ -16,6 +18,28 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/random', (req, res, next) => {
+  punkAPI.getRandom()
+  .then((beers) => {
+    console.log(beers);
+    res.render('random', {beers: beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
+app.get('/beers', (req, res, next) => {
+
+  punkAPI.getBeers()
+
+  .then((beers) => {
+    console.log(beers);
+    res.render('beers', {beers: beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+});
 
 app.listen(3000);
